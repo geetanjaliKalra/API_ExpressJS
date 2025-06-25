6f4b83356bc49e6a93d711b5f3f4322ae2ce9292
Geetanjali <gkalra88@gmail.com>
2025-06-25 13:00:47 +0400

    post method added

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
 const app = express();
+app.use(express.json());
 
 const users = [
-  { id: 1, name: 'Alice' },
-  { id: 2, name: 'Bob' }
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Bob" },
 ];
+
+let id = 3;
 
 app.get("/users", (req, res) => {
   res.json(users);
@@ -12,16 +15,23 @@ app.get("/users", (req, res) => {
 
 app.get("/users/:id", (req, res) => {
   const userId = req.params.id;
-  console.log('request recived to get user details for id: ', userId);
+  console.log("request recived to get user details for id: ", userId);
   const user = users.filter((u) => u.id == userId);
-  
-  if(!user.length) {
-    console.log('No User found with id: ', userId);
-    return res.json('No User found with id: '+ userId);
-    
+
+  if (!user.length) {
+    console.log("No User found with id: ", userId);
+    return res.json("No User found with id: " + userId);
   }
   return res.json(user[0]);
+});
 
+app.post("/users", (req, res) => {
+  const { name, mobile } = req.body;
+  const newUser = {id: id++, name, mobile}
+  users.push(newUser);
+  console.log('user array ', users);
+  
+  res.status(201).json(newUser);
 });
 
 try {
