dd2b58eaa3f5bfc5d0395017408d4c232b57c303
Geetanjali <gkalra88@gmail.com>
2025-06-25 14:53:20 +0400

    Put_Patch

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-const users = [
+let users = [
   { id: 1, name: "Alice" },
   { id: 2, name: "Bob" },
 ];
@@ -26,12 +26,24 @@ app.get("/users/:id", (req, res) => {
 });
 
 app.post("/users", (req, res) => {
-  const { name, mobile } = req.body;
-  const newUser = {id: id++, name, mobile}
-  users.push(newUser);
-  console.log('user array ', users);
-  
-  res.status(201).json(newUser);
+  try {
+    const { name, mobile } = req.body;
+    if (!name || !mobile) {
+      return res
+        .status(400)
+        .json({ error: true, message: "either name or mobile is missing" });
+    }
+    const newUser = { id: id++, name, mobile };
+    users.push(newUser);
+
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.log(error.message);
+
+    return res
+      .status(500)
+      .json({ error: true, message: error.message || "internal server error" });
+  }
 });
 
 try {
@@ -41,3 +53,59 @@ try {
 } catch (error) {
   console.error("inside catch", error);
 }
+
+app.delete("/users/:id", (req, res) => {
+  try {
+    const userId = req.params.id;
+    if (!userId) {
+      return res
+        .status(400)
+        .json({ error: true, message: "User Id is missing" });
+    }
+    users = users.filter((u) => u.id != userId);
+    res.status(200).json({
+      error: false,
+      message: `If User with id ${userId} exists then it is deleted successfully`,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: true, message: error.message || "Internal server error" });
+  }
+});
+
+app.put("/users", (req, res) => {
+  try {
+    const { id, name, mobile } = req.body;
+    const newUser = { id, name, mobile };
+    users = users.map((u) => (u.id == id ? newUser : u));
+    res
+      .status(200)
+      .json({ error: false, message: "User updated successfully", users });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: true, message: error.message || "Internal server error" });
+  }
+});
+
+app.patch("/users", (req, res) => {
+  try {
+    const { id, name, mobile } = req.body;
+    if (!id) {
+      return res.status(400).json("user id is missing");
+    }
+    const newUser = {};
+    name ? (newUser.name = name) : newUser;
+    mobile ? (newUser.mobile = mobile) : newUser;
+
+    users = users.map((u) => (u.id == id ? { ...u, ...newUser } : u));
+    res
+      .status(200)
+      .json({ error: false, message: "User updated successfully", users });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: true, message: error.message || "Internal server error" });
+  }
+});
