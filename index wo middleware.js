66e615321e36b0a91d15c2a15bcecc914fda494a
Geetanjali <gkalra88@gmail.com>
2025-06-27 14:24:31 +0400

    without middleware

---
+++ index wo middleware.js
@@ -0,0 +1,221 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+
+const key = "abc";
+
+const creds = {
+  user: "geetu",
+  password: "pass",
+};
+
+let users = [
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Bob" },
+];
+
+let id = 3;
+
+app.get("/users", (req, res) => {
+  const authHeader = req.header("Authorization");
+  // if auth header is missing, send error
+  if (!authHeader) {
+    return res.status(400).json({ error: true, message: "Auth missing" });
+  }
+
+  const base64Value = authHeader.split(" ")[1];
+  const plainValue = Buffer.from(base64Value, "base64").toString("utf-8");
+  const user = plainValue.split(":")[0];
+  const password = plainValue.split(":")[1];
+
+  // match id password
+  if (!(user == creds.user && password == creds.password)) {
+    return res
+      .status(401)
+      .json({ error: true, message: "Invalid credentials" });
+  }
+
+  res.json(users);
+});
+
+app.get("/users/:id", (req, res) => {
+  const authHeader = req.header("Authorization");
+  // if auth header is missing, send error
+  if (!authHeader) {
+    return res.status(400).json({ error: true, message: "Auth missing" });
+  }
+
+  const base64Value = authHeader.split(" ")[1];
+  const plainValue = Buffer.from(base64Value, "base64").toString("utf-8");
+  const user = plainValue.split(":")[0];
+  const password = plainValue.split(":")[1];
+
+  // match id password
+  if (!(user == creds.user && password == creds.password)) {
+    return res
+      .status(401)
+      .json({ error: true, message: "Invalid credentials" });
+  }
+  const userId = req.params.id;
+  console.log("request recived to get user details for id: ", userId);
+  const userObj = users.filter((u) => u.id == userId);
+
+  if (!userObj.length) {
+    console.log("No User found with id: ", userId);
+    return res.json("No User found with id: " + userId);
+  }
+  return res.json(userObj[0]);
+});
+
+app.post("/users", (req, res) => {
+  try {
+    const authHeader = req.header("Authorization");
+    // if auth header is missing, send error
+    if (!authHeader) {
+      return res.status(400).json({ error: true, message: "Auth missing" });
+    }
+
+    const base64Value = authHeader.split(" ")[1];
+    const plainValue = Buffer.from(base64Value, "base64").toString("utf-8");
+    const user = plainValue.split(":")[0];
+    const password = plainValue.split(":")[1];
+
+    // match id password
+    if (!(user == creds.user && password == creds.password)) {
+      return res
+        .status(401)
+        .json({ error: true, message: "Invalid credentials" });
+    }
+    const { name, mobile } = req.body;
+    if (!name || !mobile) {
+      return res
+        .status(400)
+        .json({ error: true, message: "either name or mobile is missing" });
+    }
+    const newUser = { id: id++, name, mobile };
+    users.push(newUser);
+
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.log(error.message);
+
+    return res
+      .status(500)
+      .json({ error: true, message: error.message || "internal server error" });
+  }
+});
+
+try {
+  app.listen(3000, () => {
+    console.log("Server is up and running");
+  });
+} catch (error) {
+  console.error("inside catch", error);
+}
+
+app.delete("/users/:id", (req, res) => {
+  try {
+    const authHeader = req.header("Authorization");
+    // if auth header is missing, send error
+    if (!authHeader) {
+      return res.status(400).json({ error: true, message: "Auth missing" });
+    }
+
+    const base64Value = authHeader.split(" ")[1];
+    const plainValue = Buffer.from(base64Value, "base64").toString("utf-8");
+    const user = plainValue.split(":")[0];
+    const password = plainValue.split(":")[1];
+
+    // match id password
+    if (!(user == creds.user && password == creds.password)) {
+      return res
+        .status(401)
+        .json({ error: true, message: "Invalid credentials" });
+    }
+    const userId = req.params.id;
+    if (!userId) {
+      return res
+        .status(400)
+        .json({ error: true, message: "User Id is missing" });
+    }
+    users = users.filter((u) => u.id != userId);
+    res.status(200).json({
+      error: false,
+      message: `If User with id ${userId} exists then it is deleted successfully`,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: true, message: error.message || "Internal server error" });
+  }
+});
+
+app.put("/users", (req, res) => {
+  try {
+    const authHeader = req.header("Authorization");
+    // if auth header is missing, send error
+    if (!authHeader) {
+      return res.status(400).json({ error: true, message: "Auth missing" });
+    }
+
+    const base64Value = authHeader.split(" ")[1];
+    const plainValue = Buffer.from(base64Value, "base64").toString("utf-8");
+    const user = plainValue.split(":")[0];
+    const password = plainValue.split(":")[1];
+
+    // match id password
+    if (!(user == creds.user && password == creds.password)) {
+      return res
+        .status(401)
+        .json({ error: true, message: "Invalid credentials" });
+    }
+    const { id, name, mobile } = req.body;
+    const newUser = { id, name, mobile };
+    users = users.map((u) => (u.id == id ? newUser : u));
+    res
+      .status(200)
+      .json({ error: false, message: "User updated successfully", users });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: true, message: error.message || "Internal server error" });
+  }
+});
+
+app.patch("/users", (req, res) => {
+  try {
+    const authHeader = req.header("Authorization");
+    // if auth header is missing, send error
+    if (!authHeader) {
+      return res.status(400).json({ error: true, message: "Auth missing" });
+    }
+
+    const base64Value = authHeader.split(" ")[1];
+    const plainValue = Buffer.from(base64Value, "base64").toString("utf-8");
+    const user = plainValue.split(":")[0];
+    const password = plainValue.split(":")[1];
+
+    // match id password
+    if (!(user == creds.user && password == creds.password)) {
+      return res
+        .status(401)
+        .json({ error: true, message: "Invalid credentials" });
+    }
+    const { id, name, mobile } = req.body;
+    if (!id) {
+      return res.status(400).json("user id is missing");
+    }
+    const newUser = {};
+    name ? (newUser.name = name) : newUser;
+    mobile ? (newUser.mobile = mobile) : newUser;
+
+    users = users.map((u) => (u.id == id ? { ...u, ...newUser } : u));
+    res
+      .status(200)
+      .json({ error: false, message: "User updated successfully", users });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: true, message: error.message || "Internal server error" });
+  }
+});
